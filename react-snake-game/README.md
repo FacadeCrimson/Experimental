125ff1b91b2f03c3d6743425b8f875cfef7c6f12
Lechen Tan <tan.lechen@gmail.com>
2020-08-13 08:56:08 -0400

    relocation

---
+++ react-snake-game/README.md
@@ -0,0 +1,7 @@
+# react
+
+This is the first app I make with react: the classic Snake game.
+
+Access the game at https://facadecrimson.github.io/react/.
+
+Play with arrow keys, press any other keys to end the game, press reset button to reset the game.
---
+++ react-snake-game/precache-manifest.1fd06fe70902955883532d1ae6b47bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dcfb0d717fc201e70a211a33783a3e",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "3f8dff1a95cbb97e2d1a",
+    "url": "/react/static/css/main.af634bfe.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/react/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8dff1a95cbb97e2d1a",
+    "url": "/react/static/js/main.1b734a4b.chunk.js"
+  },
+  {
+    "revision": "e13f72c0a5b2117a7fa1",
+    "url": "/react/static/js/runtime-main.da8bee19.js"
+  }
+]);
