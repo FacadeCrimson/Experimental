cc499e5f8731e144245b60047babe75495220ed7
Lechen Tan <tan.lechen@gmail.com>
2020-12-01 19:50:32 -0500

    feat: elastic search consumer

--- kafka-learn/kafka-consumer/src/main/java/com/github/kafka/ElasticSearchConsumer.java
+++ kafka-learn/kafka-consumer/src/main/java/com/github/kafka/ElasticSearchConsumer.java
@@ -1,14 +1,32 @@
 package com.github.kafka;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import com.google.gson.JsonParser;
+
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElasticSearchConsumer {
 
@@ -32,8 +50,53 @@ public class ElasticSearchConsumer {
         return client;
     }
 
-    public static void main(String[] args){
-       
+    public static KafkaConsumer<String,String> createConsumer(){
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elasticsearch";
+        String topic = "twitter_tweets";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "20");
+        //create consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String,String>(properties);
+        
+        //subscriber consumer to topics
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+    public static void main (String[] args) throws IOException{
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+        RestHighLevelClient client = createClient();
+
+        KafkaConsumer<String,String> consumer = createConsumer();
+
+        while(true){
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(100));
+            logger.info("Received "+records.count()+" records");
+            for(ConsumerRecord<String,String> record:records){
+                String jsonString = record.value();
+                String id = extractIDfromTweet(record.value());
+                IndexRequest indexRequest = new IndexRequest("twitter","tweets",id).source(jsonString,XContentType.JSON);
+                IndexResponse indexResponse = client.index(indexRequest,RequestOptions.DEFAULT);
+                logger.info(indexResponse.getId());
+            }
+            logger.info("Committing offsets...");
+            consumer.commitSync();
+            logger.info("Offsets have been commited");
+        }
+
+        //client.close();
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+    private static String extractIDfromTweet(String tweetJson){
+        return jsonParser.parse(tweetJson).getAsJsonObject().get("id_str").getAsString();
     }
     
 }
