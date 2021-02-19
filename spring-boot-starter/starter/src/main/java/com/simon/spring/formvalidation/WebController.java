faf9442c34ff6103a98c1656bf7cd08a058e7550
Lechen Tan <tan.lechen@gmail.com>
2021-02-18 22:30:52 -0500

    form validation

---
+++ spring-boot-starter/starter/src/main/java/com/simon/spring/formvalidation/WebController.java
@@ -0,0 +1,51 @@
+package com.simon.spring.formvalidation;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+    @Data
+    public class PersonForm {
+        @NotNull
+        @Size(min=2, max=30)
+        private final String name;
+    
+        @NotNull
+        @Min(18)
+        private final Integer age;
+    }
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/results").setViewName("results");
+	}
+
+	@GetMapping("/")
+	public String showForm(PersonForm personForm) {
+		return "form";
+	}
+
+	@PostMapping("/")
+	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return "form";
+		}
+
+		return "redirect:/results";
+	}
+}
