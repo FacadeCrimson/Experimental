156a64e85fe4b56aad53e5e18497b65eaffbdf46
Lechen Tan <tan.lechen@gmail.com>
2021-02-21 20:34:02 -0500

    async method

---
+++ spring-boot-starter/starter/src/main/java/com/simon/spring/asyncmethod/User.java
@@ -0,0 +1,19 @@
+package com.simon.spring.asyncmethod;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class User {
+
+  private String name;
+  private String blog;
+
+  @Override
+  public String toString() {
+    return "User [name=" + name + ", blog=" + blog + "]";
+  }
+
+}
